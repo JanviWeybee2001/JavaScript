c2441dd5caed4e1cb462e2ffb88cd55283d7a608
janvi desai <janvi@webtheuniverse.com>
2022-06-22 15:00:19 +0530

    paractical-1 done

---
+++ P1.js
@@ -0,0 +1,10 @@
+alert("hello world!");
+
+
+let js = 'amazing';
+// if (js === 'amazing') alert('javascript is FUN');
+
+js = 'boaring';
+// if (js === 'amazing') alert('javascript is FUN');
+
+console.log(40+8+25-9);
